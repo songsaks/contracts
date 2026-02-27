e262cdd7649bb8bd296e3bd059c2182e4e0d5132
songsak <aeekae@gmail.com>
2026-02-27 19:54:58 +0700

    stocks

--- stocks/urls.py
+++ stocks/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('portfolio/<int:pk>/delete/', views.delete_from_portfolio, name='delete_from_portfolio'),
     path('recommendations/', views.recommendations, name='recommendations'),
     path('macro/', views.macro_economy, name='macro'),
+    path('momentum/', views.momentum_scanner, name='momentum_scanner'),
 ]
