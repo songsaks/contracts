39a0dc7de6b690ce2940000bb6eead3e7d6b310e
songsak <aeekae@gmail.com>
2026-03-11 09:42:10 +0700

    chatbot

--- config/asgi.py
+++ config/asgi.py
@@ -8,14 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# กำหนดสิทธิ์และเตรียม Django แอพหลักให้พร้อมทำงานก่อนที่จะเรียกใช้งานแอพย่อยอื่น ๆ
+django_asgi_app = get_asgi_application()
+
+# ตอนนี้นำเข้า Routing ของ Channels ได้เพื่อหลีกเลี่ยง AppRegistryNotReady Error
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import pms.routing
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # รวมเส้นทาง WebSocket จากทุกแอปเข้าด้วยกัน
 combined_websocket_urls = (
@@ -24,7 +28,7 @@ combined_websocket_urls = (
 )
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             combined_websocket_urls
